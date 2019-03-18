e464191f5f758363bdbe039740eb6c093161ea14
Lucas <lukegauldin93@gmail.com>
2019-03-18 12:46:30 -0400

    Starting commit

---
+++ MicroLab3.ino
@@ -0,0 +1,32 @@
+// Visual Micro is in vMicro>General>Tutorial Mode
+// 
+/*
+    Name:       MicroLab3.ino
+    Created:	3/11/2019 12:24:15 PM
+    Author:     DESKTOP-IGB49L9\lukeg
+*/
+
+// Define User Types below here or use a .h file
+//
+
+
+// Define Function Prototypes that use User Types below here or use a .h file
+//
+
+
+// Define Functions below here or use other .ino or cpp files
+//
+
+// The setup() function runs once each time the micro-controller starts
+void setup()
+{
+
+
+}
+
+// Add the main program code into the continuous loop() function
+void loop()
+{
+
+
+}
