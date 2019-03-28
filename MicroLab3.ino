019e045e127f40c13ec1110948e14e8c6f9f34dd
Lucas <lukegauldin93@gmail.com>
2019-03-28 16:27:50 -0400

    finished record and most of menu

--- MicroLab3.ino
+++ MicroLab3.ino
@@ -13,9 +13,9 @@
 #pragma endregion
 
 #pragma region Modes
-#define IDLE 0
-#define RECORD 1
-#define PLAY 2
+#define MODE_IDLE 0
+#define MODE_RECORD 1
+#define MODE_PLAY 2
 #pragma endregion
 
 #pragma region IO Pins
@@ -83,7 +83,7 @@ Note song[SEGMENTS][NOTES];
 #define PRESCALER_COUNT 5
 const int cutoffs[] = { 500, 100, 10, 5, 0 };
 const int prescalers[] = { 1, 8, 64, 256, 1024 };
-bool toggle = HIGH;
+bool toggle = true;
 #pragma endregion
 
 #pragma region Playback-Record vars
@@ -94,27 +94,10 @@ int note = 0;
 int stopTime = 0;
 #pragma endregion
 
-#pragma region LCD Prototypes
-void char2LCD(const byte);
-void char2LCD(const int dataPin, const int clockPin,
-	const int ssPin, const byte ch,
-	const int byteOrder);
-void str2LCD(unsigned char *str);
-void turnBlinkingCursorOn();
-void turnBlinkingCursorOff();
-// Put cursor at location position
-// Values 0x00-0x0F for row 1, 0x40-0x4F for row 2
-void setCursorPos(unsigned char pos);
-// clear LCD and move cursor to line 1 column 1
-void clearScreen();
-// setup code for communication with LCD
-void lcdSPISetup();
-#pragma endregion
-
 // The setup() function runs once each time the micro-controller starts
 void setup()
 {
-	attachInterrupt(2, freqCalc, RISING);
+	attachInterrupt(digitalPinToInterrupt(FREQ_PIN), freqCalc, RISING);
 
 	Serial.begin(9600);  // Begin the serial monitor at 9600bps
 	Serial.println("Starting Program");
@@ -127,7 +110,7 @@ void setup()
 	bluetooth.println("U,9600,N");  // Temporarily Change the baudrate to 9600, no parity
 	// 115200 can be too fast at times for NewSoftSerial to relay the data reliably
 	bluetooth.begin(9600);  // Start bluetooth serial at 9600
-	bluetooth.print("Starting Program");
+	bluetooth.println("Starting Program");
 
 	lcdSPISetup();
 
@@ -136,38 +119,49 @@ void setup()
 	pinMode(IR_PIN, INPUT);
 	pinMode(FREQ_PIN, INPUT);
 	initPWM();
-	setPWM(PWM_FREQ, (analogRead(IR_PIN) / 1024.0) * 100);
+	setPWM(PWM_FREQ, 100);
 }
 
 // Add the main program code into the continuous loop() function
 void loop()
 {
+	
 	updateLED();
-
+	
 	if (bluetooth.available()) {
 		String line = bluetooth.readString();
-		if (line.equals("record")) mode = RECORD;
-		else if (line.equals("playback")) mode = PLAY;
+		line.trim();
+		line.toLowerCase();
+		Serial.println(line);
+		if (line.equals("record")) mode = MODE_RECORD;
+		else if (line.equals("play")) mode = MODE_PLAY;
 		else if (line.equals("help")) help();
 		else if (line.equals("clear")) clear();
-		else if (line.equals("stop")) mode = IDLE;
-		else if (line.equals("song")); // Print song segment
+		else if (line.equals("stop")) stop();
+		else if (line.equals("song")) printSegment();
 		else if (line.equals("seg")) isPressed = !isPressed; // Switch song segment
 		else error();
 	}
+
+	key = keypad.getKey();
+
+	keyWasIdle = keyIdle;
+	keyIdle = keypad.getState() == IDLE;
+
+	if (key == 'D') updateDuty(analogRead(IR_PIN));
 	
 	switch (mode) {
-	case RECORD:
+	case MODE_RECORD:
 		record();
 		break;
-	case PLAY:
+	case MODE_PLAY:
 		play();
 		break;
-	case IDLE:
-
+	case MODE_IDLE:
+		//Serial.println(frequency);
 		break;
 	default:
-		mode = IDLE;
+		mode = MODE_IDLE;
 	}
 }
 
@@ -175,36 +169,48 @@ void loop()
 void updateLED(){
 	static long lastMilli = 0;
 
-	readPWM();
 	float pause = 1000 / ((float)frequency / 2000.0) / 2;
-	Serial.println(pause);
-	bluetooth.println(pause);
-
-	//Set led brightness
-	setDuty((analogRead(IR_PIN)/1024.0)*100);
+	
 
 	//Toggle the led state
-	if(millis() - (lastMilli + pause) > 0 && toggle == HIGH)
+	if(millis() - (lastMilli + pause) > 0)
 	{
-		setPWM(PWM_FREQ, (analogRead(IR_PIN)/1024.0)*100 );
+		/*
+		Serial.println(toggle);
+		if (toggle) {
+
+			setPWM(PWM_FREQ, dutyCycle);
+			Serial.println("ON");
+		}
+		else {
+			Serial.println("OFF");
+			TCCR1B &= 0xF8;
+		}
+		*/
+
+		TCCR1A ^= _BV(COM1A1);
+
 		lastMilli = millis();
+		toggle = !toggle;
 	}
-	if(millis() - (lastMilli + pause) > 0 && toggle == LOW){
-		TCCR1B &= 0xF8;
-		lastMilli = millis();
-	}
-	toggle = !toggle;
 }
 
 //Menu
-void help(){}
+void help()
+{
+	Serial.println("HELP");
+}
 
 //Menu
-void error(){}
+void error()
+{
+	bluetooth.println("ERROR: Invalid command");
+}
 
 //Bullet 1
 void record()
 {
+	//Serial.println("RECORDING");
 	if (keyWasIdle && !keyIdle) { // Rising Edge
 		Serial.println(key);
 		key = key - ASCII0;
@@ -217,19 +223,22 @@ void record()
 	}
 	else if (!keyWasIdle && keyIdle) { // Falling Edge
 		if (measuring) {
-			Serial.println("Falling Edge");
 			song[segment][note].duration = millis() - pressTime;
+
+			Serial.println("Falling Edge");
+			/*
 			Serial.print("Saving note ");
 			Serial.print(note);
 			Serial.print(" as tone ");
 			Serial.print(song[segment][note].tone);
 			Serial.println(" and duration ");
 			Serial.print(song[segment][note].duration);
+			*/
 			note++;
 			if (note >= NOTES) {
 				note = 0;
-				mode = IDLE;
-				printSegment(segment);
+				mode = MODE_IDLE;
+				Serial.println("DONE RECORDING");
 			}
 			noTone(SPEAKER_PIN);
 			measuring = false;
@@ -238,10 +247,28 @@ void record()
 }
 
 //Bullet 2
-void play(){}
+void play()
+{
+	Serial.println("PLAY");
+}
 
 //QOL
-void stop(){}
+void stop()
+{
+	Serial.println("STOP");
+	switch (mode) {
+	case MODE_RECORD:
+		bluetooth.println("Leaving record mode");
+		break;
+	case MODE_PLAY:
+		bluetooth.println("Leaving playback mode");
+		break;
+	default:
+		bluetooth.println("Nothing to stop, returning to idle");
+	}
+
+	mode = MODE_IDLE;
+}
 
 //Bullet 3 partial
 void printLCD(){}
@@ -378,25 +405,30 @@ void initSeg(int seg) {
 	}
 }
 
-void printSegment(int seg) {
-	Serial.print(song[seg][0].tone);
-	for (int i = 1; i < NOTES; i++) {
-		Serial.print(", ");
-		Serial.print(song[seg][i].tone);
+void printSegment() {
+	bluetooth.print("Current segment: ");
+	bluetooth.println(segment);
+	for (int j = 0; j < SEGMENTS; j++) {
+		bluetooth.print("\nSegment ");
+		bluetooth.println(j);
+		for (int i = 0; i < NOTES; i++) {
+			Note note = song[j][i];
+			if (note.duration == 0) {
+				if (i == 0) bluetooth.println("EMPTY");
+				break;
+			}
+			bluetooth.print("Tone: ");
+			bluetooth.print(note.tone + 1);
+			bluetooth.print("\tDuration: ");
+			bluetooth.println(note.duration);
+		}
 	}
 }
 #pragma endregion
 
 #pragma region PWM Funcs
-void readPWM() {
-	long totalHigh = pulseIn(FREQ_PIN, HIGH);
-	long totalLow = pulseIn(FREQ_PIN, LOW);
-	frequency = 1000000 / (totalHigh + totalLow);
-	//avgDuty = (totalHigh / (float)(totalHigh + totalLow)) * 100;
-}
-
 void initPWM() {
-	TCCR1A = _BV(COM1A1) | _BV(COM1B1) | _BV(WGM11);
+	TCCR1A = _BV(COM1A1) | _BV(WGM11); //_BV(COM1B1) |
 	TCCR1B = _BV(WGM13) | _BV(WGM12) | _BV(CS12) | _BV(CS10);
 	TOP = 0xFFFF;
 	DUTY = 0x0;
@@ -441,8 +473,8 @@ void setPWM(double frequency, double duty) {
 	if (t > 0xFFFF) t = 0xFFFF;
 
 	TOP = t;
-	DUTY = (duty / 100) * TOP;
-
+	updateDuty(duty);
+	/*
 	//print data
 	Serial.println("Setting Frequency and Duty");
 	Serial.print("TOP:");
@@ -453,10 +485,11 @@ void setPWM(double frequency, double duty) {
 
 	Serial.print("\tSCALE:");
 	Serial.println(scale);
+	*/
 }
 
-void setDuty(double duty) {
-	//SET duty cycle
+void updateDuty(double duty) {
 	DUTY = (duty / 100) * TOP;
+	Serial.println("UPDATE DUTY CYCLE");
 }
 #pragma endregion
